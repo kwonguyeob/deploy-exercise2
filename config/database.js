befadf9d626e938b51a6b4d3dd80ae9d50b74966
joneconsulting <34033082+joneconsulting@users.noreply.github.com>
2020-08-13 15:26:15 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: 'mydb.c7dzg2ten1qo.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'test1234',
-  database: 'mydb'
+  host: '[수강생의 RDS 주소]',
+  user: '[수강생의 RDS user]',
+  password: '[수강생의 RDS password]',
+  database: '[수강생의 RDS DB]'
 }
